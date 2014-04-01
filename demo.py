4ed4ecb77b34a76d144ddec98b7bf7f28bc1f6d6
hongquan <meoconhocxuong_tq92@yahoo.com>
2014-04-01 09:12:59 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,57 +1,48 @@
-import flask, flask.views
-import os
-import functools
-app = flask.Flask(__name__)
-app.secret_key = "bacon"
-
-users = {'jake':'bacon'}
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import urllib2
 import os
 import re 
 import sys
-from BeautifulSoup import BeautifulSoup, NavigableString
-import httplib2
-from multiprocessing import Process
-opener = urllib2.build_opener()
-from timeout import timeout
-urllib2.install_opener(opener)
-
-def getdata(url):
-    soup = str((urllib2.urlopen(url).read()))
-    xml=re.search('file=(.*?)"', soup).group(1)
-    print xml
-    mp3 = str((urllib2.urlopen(xml).read()))
-    data=str(str(BeautifulSoup(mp3).findAll('location')[0]).split('<![CDATA[')[1]).split(']]')[0]
-    infor=str(str(BeautifulSoup(mp3).findAll('info')[0]).split('<![CDATA[')[1]).split(']]')[0]
-    print data
-    title=str(str(str(BeautifulSoup(mp3).findAll('title')[0]).split('<![CDATA[')[1]).split(']]')[0]).strip()
-    return (data,title,infor)
-        
-
+import threading
+from threading import Thread
+import math
+import random
+import requests
+import time
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs, Verbose)
+        self._return = None
+    def run(self):
+        if self._Thread__target is not None:
+            self._return = self._Thread__target(*self._Thread__args,
+                                                **self._Thread__kwargs)
+    def join(self):
+        Thread.join(self)
+        return self._return
+url='https://mp3.zing.vn/'
+def add(url):
+    time.sleep(random.random())
+    print requests.get('http://50k.itcenter.vn/website-SEO-Internet/7463/b%C3%A1n-account-mua-ban-like-10k-coin-tr%E1%BB%9F-l%C3%AAn-cho-ai-c%C3%B3-nhu-c%E1%BA%A7u-t%C4%83ng')
+    time.sleep(random.random())
+    print requests.get('http://50k.itcenter.vn/Marketing-Quang-cao/7530/C%C3%A0y-%C4%91i%E1%BB%83m-tr%C3%AAn-addmefast-12000-point-nhanh-ch%C3%B3ng-kh%C3%B4ng-b%E1%BB%8B-block')
+    time.sleep(random.random())
     
-class Main(flask.views.MethodView):
-    def get(self):
-        return flask.render_template('index.html')
+    print requests.get('http://50k.itcenter.vn/website-SEO-Internet/7403/%C4%91%C4%83ng-%E1%BA%A3nh-theo-ch%E1%BB%A7-%C4%91%E1%BB%81-l%C3%AAn-Facebook-page-c%E1%BB%A7a-b%E1%BA%A1n-v%E1%BB%9Bi-gi%C3%A1-50000-VN%C4%90-hay')
     
-    def post(self):
-  
-        username = flask.request.form['username']
-        if str(username).strip()=='' or 'http' not in str(username):
-            return flask.render_template('index.html')
+    
+t=1
+while t<100000:
+    processes = []
+    for i in range(1,10):
+        if t<10000:
+            processes.append(ThreadWithReturnValue(target=add, args=(url,), name='addstring'))
+            t+=1
         else:
-            print username
-            link=getdata(username)
-            #return '{% extends "base.html" %}{% block body %}<a href="'+link[0]+'"> '+link[1]+' </a>{%endblock%}'
-            return flask.render_template('index.html',title=link[1],value=str(link[0]).replace('://', '://download.'),
-                                         link=link[2])
-app.add_url_rule('/',
-                 view_func=Main.as_view('index'),
-                 methods=["GET", "POST"])
-
-app.debug = True
-cmd = "kill -9 `fuser -n tcp 15000`"
-os.system(cmd)
-app.run()
+            break
+    print 'so thread '+str(len(processes))
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
